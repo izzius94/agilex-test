47ae824cf864ddd428fcf8087eefdd2bcd72e1ba
izzius94 <izzius94@outlook.com>
2025-02-18 12:13:19 +0100

    feat: added route to place an order

--- code/routes/api.php
+++ code/routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login'])->name('login');
+
+Route::prefix('orders')->middleware('auth')->name('orders')->group(function () {
+    Route::post('', [OrderController::class, 'store'])->name('store');
+});
